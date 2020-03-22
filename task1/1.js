4cd5eb53f55d3fa23dde11facf840c4bb0186b67
Dmitry <sax-ru@mail.ru>
2020-03-23 01:02:49 +0300

    task 1

--- task1/1.js
+++ task1/1.js
@@ -1,4 +1,29 @@
-const arr = ["1", "2", "3"];
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let qwestion1_1 = prompt("Введите обязательную статью расходов в этом месяце");
+let qwestion1_2 = prompt("Во сколько обойдется?");
+
+let expenses = {
+    [qwestion1_1]: qwestion1_2,
+};
+
+let qwestion2_1 = prompt("Введите обязательную статью расходов в этом месяце");
+let qwestion2_2 = prompt("Во сколько обойдется?");
+
+expenses[qwestion2_1] = qwestion2_2;
 
 
-console.log(arr[2]);
+let appData = {
+    cash: money,
+    timeData: time,
+    expenses,
+    optionalExpenses: "",
+    income: "",
+    savings: false,
+
+};
+
+alert(`бюджет на 1 день ${appData.cash / 30} руб.`);
+
+console.log(appData);
