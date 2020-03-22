24a773c378838dcc1815eaa2d13fb1fbb7eb6aad
Dmitry <sax-ru@mail.ru>
2020-03-23 01:39:22 +0300

    task 1 fix

--- task1/1.js
+++ task1/1.js
@@ -1,28 +1,29 @@
-let money = prompt("Ваш бюджет на месяц?");
-let time = prompt("Введите дату в формате YYYY-MM-DD");
+"use strict";
 
-let qwestion1_1 = prompt("Введите обязательную статью расходов в этом месяце");
-let qwestion1_2 = prompt("Во сколько обойдется?");
-
-let expenses = {
-    [qwestion1_1]: qwestion1_2,
-};
-
-let qwestion2_1 = prompt("Введите обязательную статью расходов в этом месяце");
-let qwestion2_2 = prompt("Во сколько обойдется?");
-
-expenses[qwestion2_1] = qwestion2_2;
-
+let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
     cash: money,
     timeData: time,
-    expenses,
-    optionalExpenses: "",
-    income: "",
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
     savings: false,
-
 };
+
+
+let question1_1 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let question1_2 = prompt("Во сколько обойдется?", "");
+
+appData.expenses[question1_1] = question1_2;
+
+
+let question2_1 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let question2_2 = prompt("Во сколько обойдется?", "");
+
+appData.expenses[question2_1] = question2_2;
+
 
 alert(`бюджет на 1 день ${appData.cash / 30} руб.`);
 
