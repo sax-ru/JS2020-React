74d32f8de5db8c1214999afd1f559717f5f07682
Dmitry <sax-ru@mail.ru>
2020-04-26 21:12:25 +0300

    Task 14 add second task

---
+++ task14/task2/js/script.js
@@ -0,0 +1,33 @@
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+
+    function catchData() {
+
+        return new Promise(function (resolve, reject) {
+            let request = new XMLHttpRequest();
+
+            request.open('GET', 'js/current.json');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send();
+
+            request.onload = function () {
+                if (request.readyState === 4 && request.status === 200) {
+                    console.log(this);
+                    resolve(this.response);
+                } else {
+                    reject();
+                }
+            };
+        });
+    }
+
+    catchData()
+        .then(response => {
+        console.log(response);
+        let data = JSON.parse(response);
+        inputUsd.value = inputRub.value / data.usd;
+    })
+        .catch(() => inputUsd.value = "Что-то пошло не так")
+});
